454ea004e954bdb9f2eee65b785862b06e6f8de3
MET41906 <47404872+MET41906@users.noreply.github.com>
2019-02-12 15:40:16 -0800

    newest

--- .vscode/cquery_cached_index/c@@users@streulic@documents@github@team06/src@autoroutines.c
+++ .vscode/cquery_cached_index/c@@users@streulic@documents@github@team06/src@autoroutines.c
@@ -18,8 +18,8 @@ void compAutoRedRight(){
      // for 10 meter = 394" at speed 60            // turn at seed 30 for 90 degree angle right turn
     // After turn drive a bit more forward
   driveForDistancePID(12, 50);
-  pivotTurn(0, 45, 10);
-  driveForDistancePID(36, 30);
+  pivotTurn(0, 40, 5);
+  driveForDistancePID(50, 30);
   liftMove(30);
   delay(2000);
   liftMove(0);
